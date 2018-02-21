0cad85b5c497894447044b7385f96ad84e2792de
Yasir Cheema <ycheema@uwm.edu>
2018-02-20 18:25:53 -0600

    final changes

---
+++ Week4/Week4-Project/src/main/java/edu/wctc/ycheema/week4/project/model/Address.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wctc.ycheema.week4.project.model;
+
+/**
+ *
+ * @author Dell
+ */
+public class Address {
+    
+    private String Street;
+    private String city;
+    private String state;
+    private String zip;
+
+    public Address(String Street, String city, String state, String zip) {
+        this.Street = Street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public String getStreet() {
+        return Street;
+    }
+
+    public void setStreet(String Street) {
+        this.Street = Street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+    
+    
+    
+}
---
+++ Week4/Week4-Project/src/main/java/edu/wctc/ycheema/week4/project/model/Name.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wctc.ycheema.week4.project.model;
+
+/**
+ *
+ * @author Dell
+ */
+public class Name {
+    
+        private String id;
+	private String first;
+	private String last;
+        private Address address;
+        
+        public Name(String id, String first, String last, Address address) {
+		this.id = id;
+		this.first = first;
+		this.last = last;
+                this.address = address;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirst() {
+		return first;
+	}
+
+	public void setFirst(String first) {
+		this.first = first;
+	}
+
+	public String getLast() {
+		return last;
+	}
+
+	public void setLast(String last) {
+		this.last = last;
+	}
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+        
+    
+}
